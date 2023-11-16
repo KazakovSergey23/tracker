e702c61e73b74832042454849926c577c449f1cb
Сергей Казаков <Leemmbo@mail.ru>
2023-11-16 11:25:35 +0300

    7.2 Создание объекта

--- src/main/java/ru/oop/Cat.java
+++
@@ -1,9 +0,0 @@
-package ru.oop;
-
-public class Cat {
-    public static void main(String[] args) {
-        Cat polkan = new Cat();
-        Cat sharik = new Cat();
-        Cat zhychka = new Cat();
-    }
-}
