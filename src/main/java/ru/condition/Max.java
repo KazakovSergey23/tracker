54a08eb4a6adaf55564c6f840989783468f83f4e
Сергей Казаков <Leemmbo@mail.ru>
2023-11-20 22:42:53 +0300

    3.7 Тернарное сравнение

---
+++ src/main/java/ru/condition/Max.java
@@ -0,0 +1,9 @@
+package ru.condition;
+
+public class Max {
+
+    public static int max(int left, int right) {
+        return left >= right ? left : right;
+    }
+}
+
---
+++ src/test/java/ru/tracker/MaxTest.java
@@ -0,0 +1,33 @@
+package ru.tracker;
+
+import org.junit.jupiter.api.Test;
+import ru.condition.Max;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MaxTest {
+    @Test
+    void whenMax1To2Then2() {
+        int left = 1;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void whenMax2To2Then2() {
+        int left = 2;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 2;
+        assertThat(result).isEqualTo(expected);
+    }
+    @Test
+    void whenMax3To2Then3() {
+        int left = 3;
+        int right = 2;
+        int result = Max.max(left, right);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+}
