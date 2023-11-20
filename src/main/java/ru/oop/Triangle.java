028b19742e3cfa36b50380b93c9b589ed7d9e1b7
Сергей Казаков <Leemmbo@mail.ru>
2023-11-20 22:16:46 +0300

    7.13 Рефакторинг расстояние между точками

---
+++ src/main/java/ru/oop/Triangle.java
@@ -0,0 +1,34 @@
+package ru.oop;
+
+import static java.lang.Math.sqrt;
+public class Triangle {
+    private Point first;
+    private Point second;
+    private Point third;
+
+    public Triangle(Point first, Point second, Point third) {
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    public double semiPerimeter(double a, double b, double c) {
+        return (a + b + c) / 2;
+    }
+
+    public boolean exist(double ab, double ac, double bc) {
+        return ab + ac > bc || ab + bc > ac || bc + ac > ab;
+    }
+
+    public double area() {
+        double rsl = 0;
+        double ab = first.distance(second);
+        double ac = first.distance(third);
+        double bc = second.distance(third);
+        if (this.exist(ab, ac, bc)) {
+            double p = semiPerimeter(ab, ac, bc);
+            rsl = sqrt(p * (p - ab) * (p - ac) * (p - bc));
+        }
+        return rsl;
+    }
+}
