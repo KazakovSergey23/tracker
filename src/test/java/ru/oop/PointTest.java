44fa03d08f9186fd7e10935caaea720aa0b05ba6
Сергей Казаков <Leemmbo@mail.ru>
2023-11-23 18:19:11 +0300

    7.13 Рефакторинг расстояние между точками

--- src/test/java/ru/oop/PointTest.java
+++ src/test/java/ru/oop/PointTest.java
@@ -1,28 +1,31 @@
-package ru.tracker;
+package ru.oop;
 import org.junit.jupiter.api.Test;
-import ru.oop.Point;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.assertj.core.api.Assertions.assertThat;
 public class PointTest {
     @Test
     public void whenThisX1Y1X1Y1() {
         Point charger = new Point(1, 1);
         Point another = new Point(1, 1);
+        double res = charger.distance(another);
         double distance = 0.0;
-        assertThat(charger.distance(another)).isEqualTo(distance);
+        assertEquals(distance, res, 0.01);
     }
     @Test
     public void whenThisX1Y2X2Y1() {
         Point charger = new Point(1, 2);
         Point another = new Point(2, 1);
+        double res = charger.distance(another);
         double distance = 1.4142135623730951;
-        assertThat(charger.distance(another)).isEqualTo(distance);
+        assertEquals(distance, res, 0.01);
     }
     @Test
     public void whenThisX10Y5X5Y10() {
         Point charger = new Point(10, 5);
         Point another = new Point(5, 10);
+        double res = charger.distance(another);
         double distance = 7.0710678118654755;
-        assertThat(charger.distance(another)).isEqualTo(distance);
+        assertEquals(distance, res, 0.01);
+
     }
 }
