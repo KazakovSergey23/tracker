c0664dfaaf3f7b4d827a2f1a3adaa27ab32cd981
Сергей Казаков <Leemmbo@mail.ru>
2023-11-25 12:13:22 +0300

    7.16 Расстояние между точками в трехмерном пространстве

--- src/main/java/ru/oop/Point.java
+++ src/main/java/ru/oop/Point.java
@@ -6,10 +6,17 @@ import static java.lang.Math.sqrt;
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     public Point() {
@@ -19,10 +26,17 @@ public class Point {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
+    }
+
     public static void main(String[] args) {
         Point a = new Point(10, 5);
         Point b = new Point(5, 10);
         System.out.println(a.distance(b));
+        Point c = new Point(20, 15, 10);
+        Point d = new Point(10, 5, 20);
+        System.out.println(c.distance3d(d));
     }
 
 }
--- src/test/java/ru/oop/PointTest.java
+++ src/test/java/ru/oop/PointTest.java
@@ -1,8 +1,5 @@
 package ru.oop;
 import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.*;
 
 public class PointTest {
@@ -10,25 +7,48 @@ public class PointTest {
     public void whenThisX1Y1X1Y1() {
         Point charger = new Point(1, 1);
         Point another = new Point(1, 1);
-        double actual = charger.distance(another);
         double expected = 0.0;
+        double actual = charger.distance(another);
         assertThat(actual).isEqualTo(expected,withPrecision(0.01));
     }
     @Test
     public void whenThisX1Y2X2Y1() {
         Point charger = new Point(1, 2);
         Point another = new Point(2, 1);
-        double actual = charger.distance(another);
         double expected = 1.41;
+        double actual = charger.distance(another);
         assertThat(actual).isEqualTo(expected,withPrecision(0.01));
     }
     @Test
     public void whenThisX10Y5X5Y10() {
         Point charger = new Point(10, 5);
         Point another = new Point(5, 10);
-        double actual = charger.distance(another);
         double expected = 7.07;
+        double actual = charger.distance(another);
         assertThat(actual).isEqualTo(expected,withPrecision(0.01));
-
+    }
+    @Test
+    public void whenThisX2Y3Z7X4Y6Z6() {
+        Point charger = new Point(2, 3,7);
+        Point another = new Point(4, 5,6);
+        double expected = 3.0;
+        double actual = charger.distance3d(another);
+        assertThat(actual).isEqualTo(expected,withPrecision(0.01));
+    }
+    @Test
+    public void whenThisX9Y8Z7X6Y5Z4() {
+        Point charger = new Point(9, 8, 7);
+        Point another = new Point(6, 5, 4);
+        double expected = 5.19;
+        double actual = charger.distance3d(another);
+        assertThat(actual).isEqualTo(expected,withPrecision(0.01));
+    }
+    @Test
+    public void whenThisX20Y15Z10X10Y5Z20() {
+        Point charger = new Point(20, 15, 10);
+        Point another = new Point(10, 5, 20);
+        double expected = 17.32;
+        double actual = charger.distance3d(another);
+        assertThat(actual).isEqualTo(expected,withPrecision(0.01));
     }
 }
