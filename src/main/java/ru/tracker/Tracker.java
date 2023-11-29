56f4e10c460ca639519df3953fd94e2f51c787b6
Сергей Казаков <Leemmbo@mail.ru>
2023-11-29 03:40:03 +0300

    9.7 Хранилище

--- src/main/java/ru/tracker/Tracker.java
+++ src/main/java/ru/tracker/Tracker.java
@@ -3,7 +3,7 @@ package ru.tracker;
 import java.util.Arrays;
 
 public class Tracker {
-    private static final Item[] items = new Item[100];
+    private final Item[] items = new Item[100];
     private int ids = 1;
     private int size = 0;
 
