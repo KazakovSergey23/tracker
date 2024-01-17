766b219517d15ad5f52f7410725306a4257104cc
Sergey <leemmbo@mail.ru>
2024-01-17 15:22:36 +0300

    10_23_реализация за счет шаблона проектирования стратегия

--- src/main/java/ru/tracker/ReplaceAction.java
+++ src/main/java/ru/tracker/ReplaceAction.java
@@ -3,11 +3,20 @@ package ru.tracker;
 public class ReplaceAction implements UserAction {
     @Override
     public String name() {
-        return null;
+        return "Изменить заявку";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        return false;
+        System.out.println("=== Изменение заявки ===");
+        int id = input.askInt("Введетие id: ");
+        String name = input.askStr("Введите имя: ");
+        Item item = new Item(name);
+        if (tracker.replace(id, item)) {
+            System.out.println("Звявка изменена успешно.");
+        } else {
+            System.out.println("Ошибка замены заявки.");
+        }
+        return true;
     }
 }
