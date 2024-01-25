fc3af221d6be559c84b495252284af70cb09d81b
Sergey <leemmbo@mail.ru>
2024-01-25 20:21:03 +0300

    11_4 тестирование исключений

---
+++ src/test/java/ru/ex/FactorialTest.java
@@ -0,0 +1,17 @@
+package ru.ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactorialTest {
+    @Test
+    void whenExeption() {
+        Factorial factorial = new Factorial();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () ->
+            factorial.calc(-3)
+                );
+        assertThat(exception.getMessage()).isEqualTo("Факториал отрицательных чисел существовать может существовать");
+    }
+}
