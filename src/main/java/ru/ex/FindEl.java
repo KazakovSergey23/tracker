468886522c2d9e8634716862b41fe1c5a6ba66ec
Sergey <leemmbo@mail.ru>
2024-01-31 17:41:26 +0300

    11.5 Пользовательское исключение

---
+++ src/main/java/ru/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (key != null && key.equals(value[index])) {
+                result = index;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Не найден элемент");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] str = {"Алекс", "Мария", "Катя"};
+            FindEl.indexOf(str, "Мария");
+        } catch (ElementNotFoundException elementNotFoundExceptio) {
+            elementNotFoundExceptio.printStackTrace();
+        }
+    }
+}
