4b02cb4276961aecbd16eff8460698b9baab46f3
Sergey <leemmbo@mail.ru>
2024-01-31 22:36:51 +0300

    11.2 NPE

--- src/main/java/ru/ex/FindMaxLenght.java
+++ src/main/java/ru/ex/FindMaxLenght.java
@@ -4,10 +4,9 @@ public class FindMaxLenght {
     public static void main(String[] args) {
         String[] shops = {"Ebay", null, "Amazon", null, "Ozon"};
         int max = 0;
-        for (String str: shops) {
-            String element = str;
-            if (element != null && element.length() > max) {
-                max = element.length();
+        for (String shop: shops) {
+            if (shop != null && shop.length() > max) {
+                max = shop.length();
             }
         }
         System.out.println("Max lenght :" + max);
