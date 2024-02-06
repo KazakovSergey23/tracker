b249ecb976233e485a27cf7abc5880d19f8b5441
Sergey <leemmbo@mail.ru>
2024-02-06 21:05:03 +0300

    11.12 замена на if - throw

--- src/main/java/ru/ex/Find.java
+++ src/main/java/ru/ex/Find.java
@@ -2,7 +2,9 @@ package ru.ex;
 
 public class Find {
     public static String get(String[] data, int index) {
-        if (index < 0 && index > data.length) {
+        if (index < 0) {
+            throw new IllegalArgumentException("Index out of  bound");
+        } else if (index >= data.length) {
             throw new IllegalArgumentException("Index out of  bound");
         } else {
             return data[index];
--- src/test/java/ru/ex/FindTest.java
+++ src/test/java/ru/ex/FindTest.java
@@ -2,8 +2,9 @@ package ru.ex;
 
 import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
+
 import static org.junit.Assert.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FindTest {
     @Test
@@ -11,6 +12,40 @@ public class FindTest {
         String[] data = {"one", "two", "three"};
         int index = -3;
         IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Find.get(data, index));
-        assertThat(exception.getMessage())
+        assertThat(exception.getMessage()).isEqualTo("Index out of  bound");
+    }
+
+    @Test
+    void whenIndexIsGreaterThanArrayLenght(){
+        String[] data = {"one", "two", "three"};
+        int index = 10;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Find.get(data, index));
+        assertThat(exception.getMessage()).isEqualTo("Index out of  bound");
+    }
+
+    @Test
+    void whenIndexIsEqualsArrayLenght(){
+        String[] data = {"one", "two", "three"};
+        int index = 3;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> Find.get(data, index));
+        assertThat(exception.getMessage()).isEqualTo("Index out of  bound");
+    }
+
+    @Test
+    void whenIndexIsCorrect(){
+        String[] data = {"one", "two", "three"};
+        int index = 1;
+        String result = Find.get(data, index);
+        String expected = "two";
+        assertThat(expected).isEqualTo(result);
+    }
+
+    @Test
+    void whenIndexIs0ThenIsCorrect(){
+        String[] data = {"one", "two", "three"};
+        int index = 0;
+        String result = Find.get(data, index);
+        String expected = "one";
+        assertThat(expected).isEqualTo(result);
     }
 }
