3eb9cd7c815f849ecd374e46a59dc7797495543d
Sergey <leemmbo@mail.ru>
2024-02-06 20:30:19 +0300

    11.7 Error исключения

--- src/main/java/ru/ex/FactorialRecursion.java
+++ src/main/java/ru/ex/FactorialRecursion.java
@@ -2,6 +2,9 @@ package ru.ex;
 
 public class FactorialRecursion {
     public static int calc(int n) {
+        if (n <= 0) {
+            throw new IllegalArgumentException("Введете корректное значение");
+        }
         if (n == 1) {
             return n;
         }
@@ -9,7 +12,7 @@ public class FactorialRecursion {
     }
 
     public static void main(String[] args) {
-        int result = calc(2);
+        int result = calc(1);
         System.out.println(result);
     }
 }
