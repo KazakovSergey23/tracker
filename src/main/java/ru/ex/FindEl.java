2fcbfb75a4e2c7ce6ea1e23d1b119b16cb5a09fc
Sergey <leemmbo@mail.ru>
2024-02-10 13:25:13 +0300

    ошибки чекстайла

--- src/main/java/ru/ex/FindEl.java
+++ src/main/java/ru/ex/FindEl.java
@@ -4,10 +4,13 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
         for (int index = 0; index < value.length; index++) {
-            if (key != null && key.equals(value[index])) result = index;
+            if (key != null && key.equals(value[index])) {
+                result = index;
+            }
         }
-        if (result == -1) throw new ElementNotFoundException("Не найден элемент");
-
+        if (result == -1) {
+            throw new ElementNotFoundException("Не найден элемент");
+        }
         return result;
     }
 
