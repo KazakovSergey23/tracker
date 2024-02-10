6f17042b7c544fe3306f0db687a040d5b84b8aaa
Sergey <leemmbo@mail.ru>
2024-02-10 12:42:59 +0300

    ошибки чекстайла

--- src/main/java/ru/ex/BlackArray.java
+++ src/main/java/ru/ex/BlackArray.java
@@ -1,6 +1,6 @@
 package ru.ex;
 
-public class BlackArray {
+class BlackArray {
       public String[] turn(final String[] names) {
         int middle = names.length / 2;
         for (int index = 0; index < middle; index++) {
