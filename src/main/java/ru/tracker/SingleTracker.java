bcc758f2d412797d4d2d4deb7a91ef74c461512a
Sergey <leemmbo@mail.ru>
2024-02-12 14:48:33 +0300

    SingleTracker

--- src/main/java/ru/tracker/SingleTracker.java
+++ src/main/java/ru/tracker/SingleTracker.java
@@ -1,12 +1,6 @@
 package ru.tracker;
 
-import java.util.Arrays;
-import static java.lang.System.arraycopy;
-
 public final class SingleTracker {
-    private final Item[] items = new Item[100];
-    private int ids = 1;
-    private int size = 0;
     private static SingleTracker instance = null;
     private Tracker tracker = new Tracker();
 
@@ -21,59 +15,26 @@ public final class SingleTracker {
     }
 
     public Item add(Item item) {
-        item.setId(ids++);
-        items[size++] = item;
-        return item;
+        return tracker.add(item);
     }
 
     public Item[] findAll() {
-        return Arrays.copyOf(items, size);
+        return tracker.findAll();
     }
 
     public Item[] findByName(String key) {
-        Item[] result = new Item[size];
-        int index = 0;
-        for (int i = 0; i < size; i++) {
-            if (items[i].getName().equals(key)) {
-                result[index] = items[i];
-                index++;
-            }
-        }
-        return Arrays.copyOf(result, index);
+        return tracker.findByName(key);
     }
 
     public Item findById(int id) {
-        int index = indexOf(id);
-        return index != -1 ? items[index] : null;
-    }
-
-    private int indexOf(int id) {
-        int rsl = -1;
-        for (int index = 0; index < size; index++) {
-            if (items[index].getId() == id) {
-                rsl = index;
-                break;
-            }
-        }
-        return rsl;
+        return tracker.findById(id);
     }
 
     public boolean replace(int id, Item item) {
-        int index = indexOf(id);
-        if (index != -1) {
-            item.setId(id);
-            items[index] = item;
-            return true;
-        }
-        return false;
+        return tracker.replace(id, item);
     }
 
     public void delete(int id) {
-        int index = indexOf(id);
-        if (index != -1) {
-            arraycopy(items, index + 1, items, index, size - index - 1);
-            items[size - 1] = null;
-            size--;
-        }
+        tracker.delete(id);
     }
 }
