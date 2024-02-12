5f7b89345e8824fc855c25ac3e49f0924b094237
Sergey <leemmbo@mail.ru>
2024-02-12 09:22:42 +0300

    12.2 Паттерн Singleton

--- src/main/java/ru/tracker/SingleTracker.java
+++ src/main/java/ru/tracker/SingleTracker.java
@@ -6,6 +6,9 @@ import java.util.Random;
 import static java.lang.System.arraycopy;
 
 public final class SingleTracker {
+    private final Item[] items = new Item[100];
+    private int ids = 1;
+    private int size = 0;
     private static SingleTracker instance = null;
     private Tracker tracker = new Tracker();
 
@@ -20,30 +23,59 @@ public final class SingleTracker {
     }
 
     public Item add(Item item) {
-        return  tracker.add(item);
-    }
-
-    public Item findByid(int id) {
-        return null;
+        item.setId(ids++);
+        items[size++] = item;
+        return item;
     }
 
     public Item[] findAll() {
-        return null;
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
-        return null;
+        Item[] result = new Item[size];
+        int index = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                result[index] = items[i];
+                index++;
+            }
+        }
+        return Arrays.copyOf(result, index);
+    }
+
+    public Item findById(int id) {
+        int index = indexOf(id);
+        return index != -1 ? items[index] : null;
     }
 
     private int indexOf(int id) {
-        return 0;
+        int rsl = -1;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getId() == id) {
+                rsl = index;
+                break;
+            }
+        }
+        return rsl;
     }
 
     public boolean replace(int id, Item item) {
-       return true;
+        int index = indexOf(id);
+        if (index != -1) {
+            item.setId(id);
+            items[index] = item;
+            return true;
+        }
+        return false;
     }
 
     public void delete(int id) {
-
+        int index = indexOf(id);
+        if (index != -1) {
+            arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
     }
 }
