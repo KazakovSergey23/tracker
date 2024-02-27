c5a39b4b876b061cdc9c657a8e76b779bd6b9f96
Sergey <leemmbo@mail.ru>
2024-02-27 20:40:44 +0300

    13.4 телефонный справочник

--- src/main/java/ru/search/PhoneDictionary.java
+++ src/main/java/ru/search/PhoneDictionary.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PhoneDictionary {
-    private List<Person> persons = new ArrayList<>();
+    private final List<Person> persons = new ArrayList<>();
 
     public void add(Person person) {
         this.persons.add(person);
@@ -12,6 +12,15 @@ public class PhoneDictionary {
 
     public List<Person> find(String key) {
         List<Person> result = new ArrayList<>();
+        if (persons.isEmpty()) {
+            return result;
+        }
+        for (Person keys: persons) {
+            if (keys.getPhone().equals(key) || keys.getAddress().equals(key)
+                    || keys.getName().equals(key) || keys.getSurname().equals(key)) {
+                result.add(keys);
+            }
+        }
         return result;
     }
 
--- src/test/java/ru/search/PhoneDictionaryTest.java
+++ src/test/java/ru/search/PhoneDictionaryTest.java
@@ -7,6 +7,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Ivan", "Ivanov", "534872", "Bryansk"));
+        List<Person> persons = phones.find("Ivan");
+        assertThat(persons.get(0).getSurname()).isEqualTo("Ivanov");
+    }
 
+    @Test
+    public void whenFindEmptyList() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(new Person("Ivan", "Ivanov", "534872", "Bryansk"));
+        List<Person> persons = phones.find("IvanI");
+        assertThat(persons.isEmpty()).isEqualTo(true);
     }
 }
